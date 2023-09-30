9c6bc2e862cfc8b1c9ad9b4ff69b64258938f06c
ajchemist <1694505+aJchemist@users.noreply.github.com>
2023-09-30 21:33:56 +0900

    Fixed Next middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,18 @@ import type { NextRequest } from 'next/server';
 
 export default function middleware(request: NextRequest) {
     const userAgent = request.headers.get('User-Agent') || '';
-    console.log(userAgent);
-    if (userAgent.includes('kakaotalk')) {
-      // 'kakaotalk' 문자열이 포함되어 있을 경우, 특정 스크립트를 포함한 HTML 응답
-      return new Response('<script src="/inappbrowser.js"></script><script>openExternalBrowser(window.location.href)</script>', {
-        headers: { 'Content-Type': 'text/html' },
-      });
+    if (userAgent.match(/kakaotalk/i)) {
+        // 'kakaotalk' 문자열이 포함되어 있을 경우, 특정 스크립트를 포함한 HTML 응답
+        return new NextResponse(
+            '<script src="/inappbrowser.js"></script><script>openExternalBrowser(window.location.href)</script>',
+            {
+                headers: { 'Content-Type': 'text/html' },
+            });
     }
 
     return NextResponse.next()
+}
+
+export const config = {
+    matcher: '/'
 }
