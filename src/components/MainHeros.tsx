0851ddc7972bea88d3d0fa7e225c18d039ab6283
ajchemist <1694505+aJchemist@users.noreply.github.com>
2023-09-30 16:56:54 +0900

    Revised MainHeros

--- src/components/MainHeros.tsx
+++ src/components/MainHeros.tsx
@@ -8,12 +8,12 @@ export default function MainHeros({ groom, bride }: Heros) {
             <div className={`heros-profile ${noto_serif_kr.className}`}>
                 <div className={`hero-profile groom`}>
                     <div className={`next-image-container`}><Image src={`${groom.profileImage}`} alt="신랑 프로필 이미지" fill={true} style={{ objectFit: 'cover' }} /></div>
-                    <h3 className={``}>{groom.fullName}</h3>
+                    <h3 className={``}>{/* groom.fullName */}</h3>
                     <h4>신랑</h4>
                 </div>
                 <div className={`hero-profile bride`}>
                     <div className={`next-image-container`}><Image src={`${bride.profileImage}`} alt="신부 프로필 이미지" fill={true} style={{ objectFit: 'cover' }} /></div>
-                    <h3 className={``}>{bride.fullName}</h3>
+                    <h3 className={``}>{/* bride.fullName */}</h3>
                     <h4>신부</h4>
                 </div>
             </div>
