c7ae702c9fcc2575dc00b29bed9700612765e602
ajchemist <1694505+aJchemist@users.noreply.github.com>
2023-10-05 13:03:04 +0900

    Revised @/component/PageMenu

--- src/components/PageMenu.tsx
+++ src/components/PageMenu.tsx
@@ -13,14 +13,16 @@ interface PageMenuProps {
 
 export default function Page({ navPanelProps, audioPlayerProps }: PageMenuProps) {
     const sentinelRef = useRef<HTMLDivElement>(null);
+    const navRef = useRef<HTMLElement>(null);
     const [isSticky, setSticky] = useState<boolean>(false);
     const [hamburgerState, setHamburgerState] = useState<boolean>(false);
     const toggleHamburger = () => { setHamburgerState(prevState => !prevState) };
 
     useEffect(() => {
+        if (!navRef.current) return;
         if (!sentinelRef.current) return
 
-        const navHeight = sentinelRef.current.getBoundingClientRect().height;
+        const navHeight = navRef.current.getBoundingClientRect().height;
 
         const observer = new IntersectionObserver(
             ([entry]) => {
@@ -43,18 +45,19 @@ export default function Page({ navPanelProps, audioPlayerProps }: PageMenuProps)
         return () => {
             observer.disconnect()
         }
-    }, [sentinelRef])
+    }, [navRef, sentinelRef])
 
     return (
         <>
             <div ref={sentinelRef}></div>
-            <nav aria-label="Global" className={`${isSticky ? 'sticky top-0 bg-gray-100 bg-opacity-80 border-b border-gray-300 backdrop-blur-md' : ''} w-full h-12 flex items-center z-10`}>
+            <nav ref={navRef} aria-label="Global" className={`${isSticky ? 'sticky top-0 bg-gray-100 bg-opacity-80 border-b border-gray-300 backdrop-blur-md' : ''} w-full h-12 flex items-center z-10`}>
                 <div className={`grow`}></div>
-                <div className={`-mt-1 w-[44px] h-[44px] flex items-center`}>
-                    <AudioPlayer {...audioPlayerProps}/>
+                <div className={`w-[44px] h-[44px] flex items-center`}>
+                    <AudioPlayer {...audioPlayerProps} />
                 </div>
-
-                <UnbalancedTwoStackSVGHamgurger isOpen={hamburgerState} toggle={toggleHamburger} />
+                <div className={`flex items-center mt-1 pl-2.5 pr-5`}>
+                    <UnbalancedTwoStackSVGHamgurger isOpen={hamburgerState} toggle={toggleHamburger} />
+                </div>
                 <NavPanel isOpen={hamburgerState} setIsOpen={setHamburgerState} {...navPanelProps} />
             </nav>
         </>
